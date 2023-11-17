ffb8c47e6e1c331c9df867c9212e567b27faaccd
Voldex <julienvu77@gmail.com>
2023-11-17 03:02:02 +0100

    Ajout de qques parties de l'implantation de Angular Material en commentaire/à revoir

--- src/reactapp/src/App.js
+++ src/reactapp/src/App.js
@@ -4,14 +4,25 @@ import React, { Component } from 'react';
 import CommiterList from './CommiterList';
 import Home from './Home';
 import DataSender from './DataSender'
+//import { MatButton, MatProgressSpinner, MatToolbar } from '@angular/material';
 
 class App extends Component {
   render() {
     return (
         <Router>
           <Routes>
+            //<mat-toolbar color="primary">
             <Route path='/' exact={true} element={<Home />}/>
+            //</mat-toolbar>
             <Route path='/commiters' exact={true} element={<CommiterList />}/>
+            /*<Route path='/commiters' exact={true} element={
+              <div>
+                <MatButton onClick={() => {}}>
+                  List commiters
+                </MatButton>
+                {this.state.showCommiterList && <CommiterList />}
+              </div>
+            }/>*/
             <Route path='/clone-repo' exact={true} element={<DataSender />}/>
           </Routes>
         </Router>
