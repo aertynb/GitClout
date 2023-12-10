d24f74f365878d88db3d590fd795a34875ee0498
theo carrette <theo.carrette@edu.univ-eiffel.fr>
2023-12-10 14:02:32 +0100

    Ajout de material ui

--- src/reactapp/src/DataSender.js
+++ src/reactapp/src/DataSender.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import NavBar from './NavBar';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button';
 
 function DataSender() {
     const [link, setLink] = useState('');
@@ -19,16 +22,25 @@ function DataSender() {
     };
 
     return (
+        <Box
+              component="form"
+              sx={{
+                '& > :not(style)': { m: 1, width: '25ch' },
+              }}
+              noValidate
+              autoComplete="off"
+            >
         <div>
             <NavBar/>
-            <input
+            <TextField
                 type="text"
                 value={link}
                 onChange={e => setLink(e.target.value)}
                 placeholder="Enter a valid link"
             />
-            <button onClick={sendData}>Envoyer</button>
+            <Button variant="contained" size="small" onClick={sendData}>Envoyer</Button>
         </div>
+        </Box>
     );
 }
 
